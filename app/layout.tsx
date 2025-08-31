12d5d9948defd3875be006e5f6ba0bad59291240
Shaban <51951669+shabancareer@users.noreply.github.com>
2025-08-31 18:07:33 +0500

    Nav code updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"], variable: "--font-plus-jakarta-sans" });
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Clickup clone",
@@ -23,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={jakarta.variable}>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,39 +3,63 @@ import logo from "../public/assets/logo.svg";
 import styles from "./page.module.css";
 import Link from "next/link";
 import ProductDropdown from "./utilizes/productDropdown";
-import SolutionsDropdown from "./utilizes/solutionsDropdown"
+import SolutionsDropdown from "./utilizes/solutionsDropdown";
 import ResourcesDropdown from "./utilizes/resourcesDropdown";
-
 
 export default function Home() {
   return (
-    <nav className="grid grid-cols-3 gap-1">
-      <div className=" navItems ml-auto mr-3.5">
+    <nav className="grid grid-cols-3 justify-around mx-auto">
+      {/* <div className=""> */}
+      <div className=" navItems mx-auto">
         <Link href="/" className="flex items-center gap-1 w-51">
           <Image
             src={logo}
             alt="Company Logo"
             priority
-            className="h-6 w-auto"
+            className="h-6"
           />
-          <div className="h-8 w-px bg-gray-300" />
+          <div/>
           <span className="font-medium text-sm leading-none">
             The everything app, for work.
           </span>
         </Link>
       </div>
-      <div className="navItems justify-around">
+      <div className="navItems justify-between">
         <ProductDropdown />
         <SolutionsDropdown />
         <ResourcesDropdown />
-        <h1>Shaban</h1>
-        <h1>Shaban</h1>
-        
+        <Link href="">Pricing</Link>
+        <Link href="">Enterprise</Link>
       </div>
-      <div className="bg-yellow-300 navItems">
-        <h1>Shaban</h1>
-        <h1>Shaban</h1>
+      <div className=" flex gap-5 mx-auto items-center justify-between">
+        <div className="navItems ">
+          <span className="hover:bg-gray-100 p-1 rounded-sm">
+            <div className="">
+              <button className="cursor-pointer ">Contact Sales</button>
+            </div>
+          </span>
+          
+        </div>
+        <div className="navItems">
+          <span>
+            <div className="mx-auto hover:bg-gray-100 p-2 rounded-sm">
+              <button className="cursor-pointer">Login</button>
+            </div>
+          </span>
+          <span className="font-extrabold cta rounded-lg mx-2 p-2
+           backgroundColor: `rgb(var(--color-button-tertiary-background))`,
+          ">
+            <div>
+              <button
+                className=" text-sm cursor-pointer"
+              >
+                Sing Up
+              </button>
+            </div>
+          </span>
+        </div>
       </div>
+      {/* </div> */}
     </nav>
   );
 }
